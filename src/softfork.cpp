bc960851f28240d71183e0967f39f898919f7fb6
Rusty Russell <rusty@rustcorp.com.au>
2015-10-30 12:55:43 +1030

    softfork: new module currently to track softforks.
    The structure is called "BIP" for brevity, even though it could be any
    soft fork.  Currently it has simple stateless logic to support versionbits
    BIPs (BIP9).
    
    We keep pointers to the version BIPs (globals) in a table: once there
    are many this will make it clear what order they resolve in.
    
    The BIP class has only "IsActive()" which can be trivially made to
    work for IsSuperMajority()-style activation as well.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

---
+++ src/softfork.cpp
@@ -0,0 +1,261 @@
+// Copyright (c) 2015 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+#include <vector>
+#include <stdexcept>
+#include "chainparams.h"
+#include "softfork.h"
+#include "utiltime.h"
+#include "util.h"
+
+// Append new BIPs to the appropriate array, before the NULL terminator.
+static const VersionBitsBIP
+    *bit00[] = { NULL },
+    *bit01[] = { NULL },
+    *bit02[] = { NULL },
+    *bit03[] = { NULL },
+    *bit04[] = { NULL },
+    *bit05[] = { NULL },
+    *bit06[] = { NULL },
+    *bit07[] = { NULL },
+    *bit08[] = { NULL },
+    *bit09[] = { NULL },
+    *bit10[] = { NULL },
+    *bit11[] = { NULL },
+    *bit12[] = { NULL },
+    *bit13[] = { NULL },
+    *bit14[] = { NULL },
+    *bit15[] = { NULL },
+    *bit16[] = { NULL },
+    *bit17[] = { NULL },
+    *bit18[] = { NULL },
+    *bit19[] = { NULL },
+    *bit20[] = { NULL },
+    *bit21[] = { NULL },
+    *bit22[] = { NULL },
+    *bit23[] = { NULL },
+    *bit24[] = { NULL },
+    *bit25[] = { NULL },
+    *bit26[] = { NULL },
+    *bit27[] = { NULL },
+    *bit28[] = { NULL };
+
+// We keep them in separate variables so they can be different lengths.
+static const VersionBitsBIP **version_bits_table[VersionBitsBIP::NUM_VERSION_BITS] = {
+    bit00, bit01, bit02, bit03, bit04, bit05, bit06, bit07, bit08, bit09,
+    bit10, bit11, bit12, bit13, bit14, bit15, bit16, bit17, bit18, bit19,
+    bit20, bit21, bit22, bit23, bit24, bit25, bit26, bit27, bit28
+};
+
+// A pointer, for testing to override.
+static const VersionBitsBIP*** bip_table = version_bits_table;
+
+// From BIP 009, 2018 onwards
+static const unsigned int TIMEOUT_YEAR_BASE = 2018;
+static const unsigned int year_timemap[] = {
+    1514764800, 1546300800, 1577836800, 1609459200, 1640995200, 1672531200,
+    1704067200, 1735689600, 1767225600, 1798761600, 1830297600, 1861920000,
+    1893456000, 1924992000, 1956528000, 1988150400 /* 2033 */
+};
+
+static int64_t TimeoutToSeconds(unsigned int year)
+{
+    if (year < TIMEOUT_YEAR_BASE)
+        throw std::domain_error("Year below base");
+    if (year > TIMEOUT_YEAR_BASE + sizeof(year_timemap)/sizeof(year_timemap[0]))
+        throw std::domain_error("Year beyond table end");
+    return year_timemap[year - TIMEOUT_YEAR_BASE];
+}
+
+// Is this block at end of a period?
+static bool AtEndOfPeriod(unsigned int nHeight)
+{
+    const unsigned int period = Params().GetConsensus().DifficultyAdjustmentInterval();
+    return nHeight % period == period - 1;
+}
+
+// FIXME:C++11, std::array please!
+// Return version bits counts for period (of which, pblockIndex is last)
+static std::vector<unsigned int>
+TallyVersionBits(const CBlockIndex* pblockIndex)
+{
+    const unsigned int period = Params().GetConsensus().DifficultyAdjustmentInterval();
+
+    assert(AtEndOfPeriod(pblockIndex->nHeight));
+
+    std::vector<unsigned int> counts(VersionBitsBIP::NUM_VERSION_BITS);
+    for (unsigned int i = 0; i < period; i++) {
+        // Is it a valid versionbits header?  Top three bits 001.
+        if ((pblockIndex->nVersion >> 29) != 0x1)
+            continue;
+
+        // Add up all the bits.
+        for (unsigned int b = 0; b < VersionBitsBIP::NUM_VERSION_BITS; b++)
+            if ((pblockIndex->nVersion >> b) & 1)
+                counts[b]++;
+
+        // Go back.
+        pblockIndex = pblockIndex->pprev;
+    }
+
+    return counts;
+}
+
+void BIPStatus::BIPConcluded(const VersionBitsBIP* bip)
+{
+    assert(pending[bip->nBit] == bip);
+    pending[bip->nBit] = bip->pNext;
+}
+
+void BIPStatus::ActivateBIP(const VersionBitsBIP* bip)
+{
+    // Can't activate twice.
+    assert(active.find(bip) == active.end());
+
+    // Must be locked in.
+    if (locked_in.erase(bip) != 1)
+        throw std::logic_error("BIP not locked in?");
+
+    active.insert(bip);
+
+    // BIP 009:
+    //   At the that activation block and after, miners should stop
+    //   setting bit B, which may be reused for a different soft fork.
+    BIPConcluded(bip);
+}
+
+// Time out any active bips which are past their date.
+void BIPStatus::TimeoutBIP(const VersionBitsBIP* bip)
+{
+    // Can't be activated.
+    assert(active.find(bip) == active.end());
+    // Can't be locked in.
+    assert(locked_in.find(bip) == locked_in.end());
+
+    BIPConcluded(bip);
+}
+
+// Lock in a BIP which has reached consensus.
+void BIPStatus::LockInBIP(const VersionBitsBIP* bip)
+{
+    // Can't be activated.
+    assert(active.find(bip) == active.end());
+    // Can't be locked in.
+    if (!locked_in.insert(bip).second)
+        throw std::logic_error("BIP locked in twice?");
+}
+
+BIPStatus::BIPStatus(const CBlockIndex* pblockIndex)
+    : pending(std::vector<const VersionBitsBIP*>(VersionBitsBIP::NUM_VERSION_BITS))
+{
+    const int period = Params().GetConsensus().DifficultyAdjustmentInterval();
+
+    // Before end of first period, set up pending to first bit users.
+    if (!pblockIndex || pblockIndex->nHeight < period - 1) {
+        for (unsigned int b = 0; b < VersionBitsBIP::NUM_VERSION_BITS; ++b)
+            pending[b] = bip_table[b][0];
+        return;
+    }
+
+    // We only change state on period boundaries, so get end of last period.
+    if (!AtEndOfPeriod(pblockIndex->nHeight)) {
+        int past_adjust = pblockIndex->nHeight % period;
+        pblockIndex = pblockIndex->GetAncestor(pblockIndex->nHeight - past_adjust - 1);
+    }
+
+    assert(AtEndOfPeriod(pblockIndex->nHeight));
+
+    // Use block from one period ago as a base.
+    *this = BIPStatus(pblockIndex->GetAncestor(pblockIndex->nHeight - period));
+
+    // Look for lockins (consensus reached).
+    std::vector<unsigned int> counts = TallyVersionBits(pblockIndex);
+
+    // Time for this block.
+    int64_t now = pblockIndex->GetMedianTimePast();
+
+    for (unsigned int b = 0; b < VersionBitsBIP::NUM_VERSION_BITS; ++b) {
+        // Locked in.  Figure out current BIP for that bit.
+        const VersionBitsBIP* bip = pending[b];
+
+        bool success = (counts[b] >= Params().GetConsensus().nVersionBitsLockinThreshold);
+
+        // Unknown BIP?
+        if (!bip) {
+            continue;
+        }
+
+        // BIP 009:
+        //  The consensus rules related to ''locked-in'' soft fork will
+        //  be enforced in the second retarget period
+        if (find(locked_in.begin(), locked_in.end(), bip) != locked_in.end()) {
+            ActivateBIP(bip);
+            continue;
+        }
+
+        // If we got the numbers, we lock it in now.
+        if (success) {
+            LockInBIP(bip);
+            continue;
+        }
+
+        // Otherwise, check for timeout.
+        if (now >= bip->nTimeout) {
+            TimeoutBIP(bip);
+            continue;
+        }
+
+        // BIP is still waiting to be activated.
+    }
+}
+
+// The table is canonical, and nicely visual.  We derive our position on
+// first usage.
+VersionBitsBIP::VersionBitsBIP(int year,
+                               const VersionBitsBIP*** table)
+    : nTimeout(TimeoutToSeconds(year))
+{
+    if (table)
+        bip_table = table;
+
+    // Find ourselves in the table.
+    for (size_t b = 0; b < NUM_VERSION_BITS; b++) {
+        for (size_t gen = 0; bip_table[b][gen]; gen++) {
+            if (bip_table[b][gen] == this) {
+                nBit = b;
+                pNext = bip_table[b][gen + 1];
+                return;
+            }
+        }
+    }
+
+    // We're not in the table.
+    throw std::logic_error("BIP is not in the version_bits_table");
+}
+
+bool VersionBitsBIP::IsActive(const CBlockIndex* pblockIndex) const
+{
+    // Get status for all the BIPs for this block.
+    BIPStatus status(pblockIndex);
+
+    // Are we in the active set?
+    return status.active.find(this) != status.active.end();
+}
+
+int VersionForNextBlock(const CBlockIndex* pblockIndex)
+{
+    // BIP 009:
+    //   The highest 3 bits are set to 001
+    uint32_t bits = (1U << 29);
+
+    BIPStatus status(pblockIndex);
+
+    // BIP009:
+    //  Software which supports the change should begin by setting B
+    //  in all blocks mined until it is resolved.
+    for (unsigned int b = 0; b < VersionBitsBIP::NUM_VERSION_BITS; b++)
+        if (status.pending[b])
+            bits |= (1U << b);
+
+    return bits;
+}
---
+++ src/softfork.h
@@ -0,0 +1,75 @@
+// Copyright (c) 2015 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_SOFTFORK_H
+#define BITCOIN_SOFTFORK_H
+#include "chain.h"
+
+/**
+ * BIP gives you a handle for a BIP.  Its only purpose is to allow you
+ * to query whether it applies to a given block.
+ */
+struct BIP
+{
+    // Is this BIP active for this block?
+    virtual bool IsActive(const CBlockIndex* pblockIndex) const = 0;
+};
+
+/**
+ * A BIP which uses BIP9 (versionbits) to activate.  To add a new one,
+ * be sure to append it to the table in softfork.cpp.
+ */
+struct VersionBitsBIP : public BIP
+{
+    static const size_t NUM_VERSION_BITS = 29;
+
+    // Optional table arg is a hack for testing.
+    VersionBitsBIP(int year,
+                   const VersionBitsBIP*** override_table = NULL);
+
+    // Is this BIP active for this block?
+    virtual bool IsActive(const CBlockIndex* pblockIndex) const;
+
+    int64_t nTimeout;            //!< Timeout in seconds since epoch
+    unsigned int nBit;           //!< Which version bit, derived from table
+    const VersionBitsBIP* pNext; //!< Next version bit user, if any.
+};
+
+/**
+ * Status of all BIPs for a given block.
+ */
+struct BIPStatus
+{
+    BIPStatus(const CBlockIndex* pblockIndex);
+
+    // Two exclusive sets (active could include non-versionbits)
+    std::set<const BIP*> active;
+    std::set<const VersionBitsBIP*> locked_in;
+
+    // Where we're up to in version_bits_table (each of these is inactive,
+    // or locked_in).
+    // FIXME: C++11 std::array!
+    std::vector<const VersionBitsBIP*> pending;
+
+    // Activate a locked-in bip (happens 1 period after activation).
+    void ActivateBIP(const VersionBitsBIP* bip);
+
+    // Time out any active bips which are past their date.
+    void TimeoutBIP(const VersionBitsBIP* bip);
+
+    // Lock in a BIP which has reached consensus.
+    void LockInBIP(const VersionBitsBIP* bip);
+
+private:
+    // Update to next pending bip.
+    void BIPConcluded(const VersionBitsBIP* bip);
+};
+
+/**
+ * VersionForNextBlock:  As a miner, what should nVersion be for next block?
+ * @param[in] pblockIndex: the block you're building on top of.
+ */
+int VersionForNextBlock(const CBlockIndex* pblockIndex);
+
+#endif // BITCOIN_SOFTFORK_H
