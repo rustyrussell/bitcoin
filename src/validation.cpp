moosuchot/bitcoinfibre

Call UDPRelayBlock from ProcessNewBlock when appropriate

src/validation.cpp
@@ -27,6 +27,7 @@
 #include "tinyformat.h"
 #include "txdb.h"
 #include "txmempool.h"
+#include "udpnet.h"
 #include "ui_interface.h"
 #include "undo.h"
 #include "util.h"
@@ -3198,8 +3199,12 @@ static bool AcceptBlock(const std::shared_ptr<const CBlock>& pblock, CValidation
 
     // Header is valid/has work, merkle tree and segwit merkle tree are good...RELAY NOW
     // (but if it does not build on our best tip, let the SendMessages loop relay it)
-    if (!IsInitialBlockDownload() && chainActive.Tip() == pindex->pprev)
-        GetMainSignals().NewPoWValidBlock(pindex, pblock);
+    if (!IsInitialBlockDownload()) {
+        UDPRelayBlock(block);
+        if (chainActive.Tip() == pindex->pprev) {
+            GetMainSignals().NewPoWValidBlock(pindex, pblock);
+        }
+    }
 
     int nHeight = pindex->nHeight;
 

rustyrussell/bitcoin

src/validation.cpp
@@ -27,6 +27,7 @@
 #include "tinyformat.h"
 #include "txdb.h"
 #include "txmempool.h"
+#include "udpapi.h"
 #include "ui_interface.h"
 #include "undo.h"
 #include "util.h"
@@ -3201,8 +3202,12 @@ static bool AcceptBlock(const std::shared_ptr<const CBlock>& pblock, CValidation
 
     // Header is valid/has work, merkle tree and segwit merkle tree are good...RELAY NOW
     // (but if it does not build on our best tip, let the SendMessages loop relay it)
-    if (!IsInitialBlockDownload() && chainActive.Tip() == pindex->pprev)
-        GetMainSignals().NewPoWValidBlock(pindex, pblock);
+    if (!IsInitialBlockDownload()) {
+        UDPRelayBlock(block);
+        if (chainActive.Tip() == pindex->pprev) {
+            GetMainSignals().NewPoWValidBlock(pindex, pblock);
+        }
+    }
 
     int nHeight = pindex->nHeight;
 